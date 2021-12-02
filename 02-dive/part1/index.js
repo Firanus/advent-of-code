91bb2ddbd84a99dbb181791b6cb7cdad36f89bd6
Ivan Tchernev <ivan@letsdothis.com>
2021-12-02 13:19:04 +0100

    Day 2

---
+++ 02-dive/part1/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const fs = require('fs')
+
+fs.readFile(path.resolve(__dirname, '../input.txt'), 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const instructions = data.split('\n');
+  const finalPositions = instructions.reduce((acc, curr) => {
+    const [direction, sizeString] = curr.split(' ');
+    const size = parseInt(sizeString, 10);
+    if (direction === 'forward') {
+      acc.horizontal += size;
+    }
+
+    if (direction === 'down') {
+      acc.depth += size;
+    }
+
+    if (direction === 'up') {
+      acc.depth -= size;
+    }
+
+    return acc;
+  }, {horizontal: 0, depth: 0});
+
+  console.log(finalPositions);
+  console.log(finalPositions.depth * finalPositions.horizontal);
+})
---
+++ 02-dive/part2/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const fs = require('fs')
+
+fs.readFile(path.resolve(__dirname, '../input.txt'), 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const instructions = data.split('\n');
+  const finalPositions = instructions.reduce((acc, curr) => {
+    const [direction, sizeString] = curr.split(' ');
+    const size = parseInt(sizeString, 10);
+    if (direction === 'forward') {
+      acc.horizontal += size;
+      acc.depth += acc.aim * size;
+    }
+
+    if (direction === 'down') {
+      acc.aim += size;
+    }
+
+    if (direction === 'up') {
+      acc.aim -= size;
+    }
+
+    return acc;
+  }, {horizontal: 0, depth: 0, aim: 0});
+
+  console.log(finalPositions);
+  console.log(finalPositions.depth * finalPositions.horizontal);
+})
