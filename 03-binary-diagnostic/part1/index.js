426e14708dcfe2bbe5dc1075a0161b4bb46b83db
Ivan Tchernev <ivan@letsdothis.com>
2021-12-03 11:57:35 +0100

    Day 3

---
+++ 03-binary-diagnostic/part1/index.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require('fs')
+
+fs.readFile(path.resolve(__dirname, '../input.txt'), 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const binaryNumbers = data.split('\n');
+  const countOfBinaries = binaryNumbers.length;
+  const countOfOnes = binaryNumbers.reduce((acc, curr) => {
+    for(let i = 0; i < curr.length; i++) {
+      if (curr[i] === '1') {
+        acc[i] += 1;
+      }
+    }
+
+    return acc;
+  }, new Array(binaryNumbers[0].length).fill(0));
+  console.log(countOfBinaries);
+  console.log(countOfOnes);
+
+  let gammaRate = '';
+  let epsilonRate = '';
+
+  for (let i = 0; i < countOfOnes.length; i++) {
+    if (countOfOnes[i] > countOfBinaries / 2) {
+      gammaRate += '1';
+      epsilonRate += '0';
+    } else {
+      gammaRate += '0';
+      epsilonRate += '1';
+    }
+  }
+
+  console.log(gammaRate, epsilonRate);
+  console.log(parseInt(gammaRate, 2), parseInt(epsilonRate, 2))
+  console.log(parseInt(gammaRate, 2) * parseInt(epsilonRate, 2))
+})
---
+++ 03-binary-diagnostic/part2/index.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const fs = require('fs')
+
+fs.readFile(path.resolve(__dirname, '../input.txt'), 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const binaryNumbers = data.split('\n');
+  const binaryNumberLength = binaryNumbers[0].length;
+  let oxygenRating = '';
+  let co2Rating = '';
+  let holdingBinaries = binaryNumbers;
+
+  for (let i = 0; i < binaryNumberLength; i++) {
+    const positionToExamine = i;
+    const mostCommon = mostCommonInBitPosition(holdingBinaries, positionToExamine);
+    holdingBinaries = holdingBinaries.filter(bin => bin[positionToExamine] === mostCommon);
+
+    if(holdingBinaries.length === 1) {
+      oxygenRating = holdingBinaries[0];
+      break;
+    }
+  }
+
+  holdingBinaries = binaryNumbers;
+  for (let i = 0; i < binaryNumberLength; i++) {
+    const positionToExamine = i;
+    const leastCommon = leastCommonInBitPosition(holdingBinaries, positionToExamine);
+    holdingBinaries = holdingBinaries.filter(bin => bin[positionToExamine] === leastCommon);
+
+    if(holdingBinaries.length === 1) {
+      co2Rating = holdingBinaries[0];
+      break;
+    }
+  }
+
+  console.log(oxygenRating, co2Rating);
+  console.log(parseInt(oxygenRating, 2), parseInt(co2Rating, 2))
+  console.log(parseInt(oxygenRating, 2) * parseInt(co2Rating, 2))
+});
+
+const mostCommonInBitPosition = (binaryNumbers, positionToCheck) => {
+  let countOfOnes = 0;
+  for(let i =  0; i < binaryNumbers.length; i++) {
+    if (binaryNumbers[i][positionToCheck] === '1') {
+      countOfOnes += 1;
+    }
+  }
+  
+  if (countOfOnes >= binaryNumbers.length / 2) {
+    return '1';
+  } else {
+    return '0';
+  }
+}
+
+const leastCommonInBitPosition = (binaryNumbers, positionToCheck) => {
+  const mostCommon = mostCommonInBitPosition(binaryNumbers, positionToCheck);
+  if (mostCommon === '1') return '0';
+  return '1';
+}
