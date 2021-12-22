a402813989b85697d1e0647fccbbacb95d83ff43
Ivan Tchernev - Work <ivan@letsdothis.com>
2021-12-22 22:22:38 +0200

    Day 22 - Part 2

--- 22-reactor-reboot/part2/index.js
+++ 22-reactor-reboot/part2/index.js
@@ -1,61 +1,108 @@
 const path = require("path");
 const fs = require("fs");
 
-fs.readFile(path.resolve(__dirname, "./testInput.txt"), "utf8", (err, data) => {
+fs.readFile(path.resolve(__dirname, "./input.txt"), "utf8", (err, data) => {
   if (err) {
     console.error(err);
     return;
   }
   const instructions = data.split("\n").map(processInstructions);
 
-  // Let's try storing disjoint rectangular prisms. We'll store them in an array, slicing from lowest z-coordinate.
-  const cubes = {};
+  let positiveCubes = [];
+  let negativeCubes = [];
   for (let i = 0; i < instructions.length; i++) {
     const instruction = instructions[i];
     const shouldTurnOn = instruction[0];
     const coordsToChange = instruction[1];
-    const overlappingCubes = getOverlappingCubes(coordsToChange, cubes);
 
-    if (!overlappingCubes.length) {
-      if (shouldTurnOn) {
-        cubes[genKey(coordsToChange)] = coordsToChange;
-      }
+    const overlapsWithPositiveCubes = getOverlappingCubes(
+      coordsToChange,
+      positiveCubes
+    );
+    const overlapsWithNegativeCubes = getOverlappingCubes(
+      coordsToChange,
+      negativeCubes
+    );
+
+    if (shouldTurnOn) {
+      positiveCubes.push(coordsToChange);
+    }
+    positiveCubes = positiveCubes.concat(overlapsWithNegativeCubes);
+    negativeCubes = negativeCubes.concat(overlapsWithPositiveCubes);
+  }
+
+  let totalArea = 0;
+  let positiveIndex = 0;
+  let negativeIndex = 0;
+  let pullPositiveNext = true;
+  while (
+    positiveIndex < positiveCubes.length ||
+    negativeIndex < negativeCubes.length
+  ) {
+    if (positiveIndex >= positiveCubes.length) {
+      totalArea -= getCubeVolume(negativeCubes[negativeIndex]);
+      negativeIndex += 1;
+      continue;
     }
 
-    console.log(cubes);
+    if (negativeIndex >= negativeCubes.length) {
+      totalArea += getCubeVolume(positiveCubes[positiveIndex]);
+      positiveIndex += 1;
+      continue;
+    }
+
+    if (pullPositiveNext) {
+      totalArea += getCubeVolume(positiveCubes[positiveIndex]);
+      positiveIndex += 1;
+    } else {
+      totalArea -= getCubeVolume(negativeCubes[negativeIndex]);
+      negativeIndex += 1;
+    }
+    pullPositiveNext = !pullPositiveNext;
   }
+
+  console.log(totalArea);
 });
+
+const getCubeVolume = (cube) =>
+  (1 + Math.abs(cube[0][1] - cube[0][0])) *
+  (1 + Math.abs(cube[1][1] - cube[1][0])) *
+  (1 + Math.abs(cube[2][1] - cube[2][0]));
 
 const getOverlappingCubes = (instructionCoords, cubes) => {
   const overlappingCubes = [];
-  const existingCubes = Object.values(cubes);
+  const existingCubes = cubes;
   for (let i = 0; i < existingCubes.length; i++) {
     const cubeCoords = existingCubes[i];
-    if (has3DOverlap(cubeCoords, instructionCoords)) {
-      overlappingCubes.push(cubeCoords);
+    const overlapCube = getOverlapCube(cubeCoords, instructionCoords);
+    if (overlapCube) {
+      overlappingCubes.push(overlapCube);
     }
   }
 
   return overlappingCubes;
 };
 
-const has3DOverlap = (threeDCoordsA, threeDCoordsB) => {
-  return (
-    doesAxisOverlap(threeDCoordsA[0], threeDCoordsB[0]) &&
-    doesAxisOverlap(threeDCoordsA[1], threeDCoordsB[1]) &&
-    doesAxisOverlap(threeDCoordsA[2], threeDCoordsB[2])
-  );
+const getOverlapCube = (threeDCoordsA, threeDCoordsB) => {
+  const xAxisOverlap = axisOverlap(threeDCoordsA[0], threeDCoordsB[0]);
+  const yAxisOverlap = axisOverlap(threeDCoordsA[1], threeDCoordsB[1]);
+  const zAxisOverlap = axisOverlap(threeDCoordsA[2], threeDCoordsB[2]);
+
+  if (xAxisOverlap && yAxisOverlap && zAxisOverlap) {
+    return [xAxisOverlap, yAxisOverlap, zAxisOverlap];
+  }
+  return undefined;
 };
 
-const doesAxisOverlap = (oneDCoordsA, oneDCoordsB) => {
+const axisOverlap = (oneDCoordsA, oneDCoordsB) => {
   const greaterMin = Math.max(oneDCoordsA[0], oneDCoordsB[0]);
   const lesserMax = Math.min(oneDCoordsA[1], oneDCoordsB[1]);
 
-  return greaterMin <= lesserMax;
+  if (greaterMin <= lesserMax) {
+    return [greaterMin, lesserMax];
+  }
+  return undefined;
 };
-
-const genKey = (coordinates) =>
-  `${coordinates[0][0]}-${coordinates[0][1]}:${coordinates[1][0]}-${coordinates[1][1]}:${coordinates[2][0]}-${coordinates[2][1]}`;
 
 const processInstructions = (stringInput) => {
   const sections = stringInput.split(" ");
