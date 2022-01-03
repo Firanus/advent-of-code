b7e567ce3f13541d86a7d0818ee260741e9347c2
Ivan Tchernev - Work <ivan@letsdothis.com>
2022-01-03 13:30:23 +0000

    Tidied up 2021 Day 2

---
+++ 2021/02-dive/README.md
@@ -0,0 +1,19 @@
+# 2021 - Day 2
+
+[https://adventofcode.com/2021/day/2](https://adventofcode.com/2021/day/2)
+
+## Part 1
+
+To solve Part 1, run the following from the project root:
+
+```sh
+node 2021/02-dive/part1/index.js
+```
+
+## Part 2
+
+To solve Part 1, run the following from the project root:
+
+```sh
+node 2021/02-dive/part2/index.js
+```
--- 2021/02-dive/part2/index.js
+++ 2021/02-dive/part2/index.js
@@ -1,31 +1,37 @@
 const path = require("path");
-const fs = require('fs')
+const fs = require("fs");
 
-fs.readFile(path.resolve(__dirname, '../input.txt'), 'utf8' , (err, data) => {
+fs.readFile(path.resolve(__dirname, "../input.txt"), "utf8", (err, data) => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
+    return;
   }
-  const instructions = data.split('\n');
-  const finalPositions = instructions.reduce((acc, curr) => {
-    const [direction, sizeString] = curr.split(' ');
-    const size = parseInt(sizeString, 10);
-    if (direction === 'forward') {
-      acc.horizontal += size;
-      acc.depth += acc.aim * size;
-    }
+  const instructions = data.split("\n");
+  const finalPositions = instructions.reduce(
+    (acc, curr) => {
+      const [direction, sizeString] = curr.split(" ");
+      const size = parseInt(sizeString, 10);
+      if (direction === "forward") {
+        acc.horizontal += size;
+        acc.depth += acc.aim * size;
+      }
 
-    if (direction === 'down') {
-      acc.aim += size;
-    }
+      if (direction === "down") {
+        acc.aim += size;
+      }
 
-    if (direction === 'up') {
-      acc.aim -= size;
-    }
+      if (direction === "up") {
+        acc.aim -= size;
+      }
 
-    return acc;
-  }, {horizontal: 0, depth: 0, aim: 0});
+      return acc;
+    },
+    { horizontal: 0, depth: 0, aim: 0 }
+  );
 
   console.log(finalPositions);
-  console.log(finalPositions.depth * finalPositions.horizontal);
-})
+  console.log(
+    "Part 2 Answer:",
+    finalPositions.depth * finalPositions.horizontal
+  );
+});
