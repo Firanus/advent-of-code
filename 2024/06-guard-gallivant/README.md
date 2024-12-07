040aef967f19996d1bc37d13a688c04aa58d7480
Ivan Tchernev - Work <ivan@letsdothis.com>
2024-12-07 21:18:00 +0000

    2024 - Day 6 - Part 2

---
+++ 2024/06-guard-gallivant/README.md
@@ -0,0 +1,15 @@
+# 2024 - Day 6
+
+[https://adventofcode.com/2024/day/6](https://adventofcode.com/2024/day/6)
+
+Another novel problem. Part 1 is just about walking a path. Part 2 is about
+putting obstacles in it to see if it loops. Ultimately, didn't have a
+very novel solution to this, it seemed the right thing to do was to
+essentially just try every position on the path you'd already walked, but
+interesting nonetheless!
+
+To solve both parts 1 and 2, run the following from the project root:
+
+```sh
+npx ts-node 2024/06-guard-gallivant
+```
--- 2024/06-guard-gallivant/index.ts
+++ 2024/06-guard-gallivant/index.ts
@@ -1,7 +1,21 @@
 import path from "path";
 import fs from "fs";
+import { start } from "repl";
+import { get } from "http";
 
-const encodePosition = (row: number, col: number): string => `${row},${col}`;
+const encodePosition = (
+  row: number,
+  col: number,
+  direction: Direction
+): string => `${row},${col},${direction}`;
+const decodePosition = (position: string): [number, number, Direction] => {
+  const [row, col, direction] = position.split(",").map((n) => parseInt(n, 10));
+  return [row, col, direction];
+};
+const visualiseMap = (map: string[][]): void => {
+  console.log(map.map((row) => row.join("")).join("\n"));
+};
+
 enum Direction {
   Up,
   Right,
@@ -19,20 +33,67 @@ fs.readFile(
     }
 
     const map = data.split("\n").map((line) => line.split(""));
-    let row = map.findIndex((row) => row.includes("^"));
-    let col = map[row].indexOf("^");
-    let direction = Direction.Up;
+    const startRow = map.findIndex((row) => row.includes("^"));
+    const startCol = map[startRow].indexOf("^");
+    const startDirection = Direction.Up;
 
-    const visitedPositions: string[] = [];
+    const [visitedPositions] = walkPath(
+      startRow,
+      startCol,
+      startDirection,
+      map
+    );
 
-    while (!hasLeftArea(row, col, map)) {
-      visitedPositions.push(encodePosition(row, col));
-      [row, col, direction] = move(row, col, direction, map);
+    const uniqVisitedPositions = getUniqVisitedPositions(visitedPositions);
+    console.log("Part 1 Solution - ", uniqVisitedPositions.length);
+
+    let loopedPathCount = 0;
+    for (let i = 0; i < uniqVisitedPositions.length; i++) {
+      if (i % 100 === 0)
+        console.log(i, "paths walked of", uniqVisitedPositions.length);
+      if (
+        encodePosition(startRow, startCol, startDirection) ===
+        uniqVisitedPositions[i]
+      )
+        continue;
+
+      const [row, col] = decodePosition(uniqVisitedPositions[i]);
+      const newMap = map.map((row) => [...row]);
+      newMap[row][col] = "#";
+
+      const [_, hasLoop] = walkPath(startRow, startCol, startDirection, newMap);
+      if (hasLoop) loopedPathCount++;
     }
 
-    console.log("Part 1 Solution - ", uniq(visitedPositions).length);
+    console.log("Part 2 Solution - ", loopedPathCount);
   }
 );
+
+const walkPath = (
+  startRow: number,
+  startCol: number,
+  startDirection: Direction,
+  map: string[][]
+): [string[], boolean] => {
+  const visitedPositions: string[] = [];
+  let row = startRow;
+  let col = startCol;
+  let direction = startDirection;
+
+  while (!hasLeftArea(row, col, map)) {
+    if (startRow === 7 && startCol === 6) {
+      console.log("Current position", row, col, direction);
+    }
+    if (visitedPositions.includes(encodePosition(row, col, direction))) {
+      return [visitedPositions, true];
+    }
+
+    visitedPositions.push(encodePosition(row, col, direction));
+    [row, col, direction] = move(row, col, direction, map);
+  }
+
+  return [visitedPositions, false];
+};
 
 const move = (
   row: number,
@@ -43,7 +104,7 @@ const move = (
   let [nextRow, nextCol] = getNextPosition(row, col, direction);
   let currentDirection = direction;
   while (isWall(nextRow, nextCol, map)) {
-    currentDirection = turn(direction);
+    currentDirection = turn(currentDirection);
     [nextRow, nextCol] = getNextPosition(row, col, currentDirection);
   }
 
@@ -89,4 +150,11 @@ const turn = (direction: Direction): Direction => {
   }
 };
 
-const uniq = <T>(arr: T[]): T[] => Array.from(new Set(arr));
+const getUniqVisitedPositions = (visitedPositions: string[]): string[] => {
+  const positionsWithoutDirection = visitedPositions.map((p) => {
+    const [row, col, _] = decodePosition(p);
+    return `${row},${col}`;
+  });
+  const visitedPositionsSet = new Set(positionsWithoutDirection);
+  return [...visitedPositionsSet];
+};
